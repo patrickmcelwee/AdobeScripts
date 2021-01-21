c0588fa4f477df48b66813dfdc6886951bd2a03d
Patrick McElwee <pmcelwee@gmail.com>
2021-01-21 15:54:55 -0500

    Ask user to input min and max sizes

--- LayerPatternPieces.jsx
+++ LayerPatternPieces.jsx
@@ -3,18 +3,38 @@
 
 layerTextLabels(); 
 function layerTextLabels() {
+  //Make certain that user interaction (display of dialogs, etc.) is turned on.
+  app.userInteractionLevel = UserInteractionLevel.DISPLAYALERTS;
+
   var myDocument = app.activeDocument;
-  var minSize = 0;
-  var maxSize = 24;
+
+  var dialog = new Window("dialog", "Enter Sizes");
+
+  var minSize = parseInt(prompt("Enter the minimum size", 0));
+  if (!minSize || isNaN(minSize) || minSize < 0 || minSize % 2 !== 0) {
+    throw("Not a valid size, must be multiple of 2: " + minSize)
+  }
+  var maxSize = parseInt(prompt("Enter the maximum size", 24));
   for (size=minSize; size <= maxSize; size += 2) {
     var sizeString = size.toString();
     // moves "size" labels to "Size ${size}" layer
-    var targetLayer = myDocument.layers.getByName("Size " + sizeString);
-    for (i=0; i < myDocument.textFrames.length; i++) {
-      var thisFrame = myDocument.textFrames[i];
-      if (thisFrame.contents === sizeString) {
-        thisFrame.move(targetLayer, ElementPlacement.PLACEATEND);
+    var targetLayer;
+    try {
+      targetLayer = myDocument.layers.getByName("Size " + sizeString);
+    } catch(e) {
+      alert("Layer 'Size " + sizeString + "' does not exist. Adding it now. " + e);
+      targetLayer = myDocument.layers.add();
+      targetLayer.name = "Size " + sizeString;
+    }
+    if (targetLayer) {
+      for (i=0; i < myDocument.textFrames.length; i++) {
+        var thisFrame = myDocument.textFrames[i];
+        if (thisFrame.contents === sizeString) {
+          thisFrame.move(targetLayer, ElementPlacement.PLACEATEND);
+        }
       }
+    } else {
+      alert("No such layer: Size " + sizeString);
     }
   }
 }
